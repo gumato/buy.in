c83d8464a80ded4d4304f897729d6957de5a9f3b
pricilla gumato sharamo <gumatopricilla22@gmail.com>
2019-06-06 18:01:38 +0300

    Add admin page

--- buyapp/models.py
+++ buyapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=300)
+
+class Product(models.Model):
+    category = models.ForeignKey(Category,related_name='products')
+    name = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='products/%Y/%m/%d',blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        ordering = ('-created',)
+    def __str__(self):
+        return self.name
+
