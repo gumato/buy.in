6c2dd3cbaea44c52aed3bab5362e9ab41268ea14
pricilla gumato sharamo <gumatopricilla22@gmail.com>
2019-06-06 16:19:41 +0300

    installing bootstrap

--- Buyin/settings.py
+++ Buyin/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'buyapp.apps.BuyappConfig',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- buyapp/views.py
+++ buyapp/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http  import HttpResponse
+import datetime as dt
 
 # Create your views here.
 def index(request):
     date = dt.date.today()
-    products = Products.objects.all()
-    profiles = Profile.objects.all()
-    return render(request,'index.html',{"date": date,"products":products})
+    # products = Products.objects.all()
+    # profiles = Profile.objects.all()
+    return render(request,'index.html')
