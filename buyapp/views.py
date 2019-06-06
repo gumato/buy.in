2fa25030729c5a400d221b00de2f7176d253e8af
pricilla gumato sharamo <gumatopricilla22@gmail.com>
2019-06-06 15:55:58 +0300

    Adding url file

--- buyapp/views.py
+++ buyapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http  import HttpResponse
 
 # Create your views here.
+def index(request):
+    date = dt.date.today()
+    products = Products.objects.all()
+    profiles = Profile.objects.all()
+    return render(request,'index.html',{"date": date,"products":products})
